344dc1dfe07bc89e5e1431231e85ee88ea2e04e0
Ivan Valadez <ivan_valadez_11@hotmail.com>
2019-06-04 23:16:35 -0500

    1° Commit, addition of HelloWorld file.

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+    public static void test(){
+        System.out.println("Hello Due");
+    }
+
+    public static void main(String []args) {
+        System.out.println("Hello World"); // prints Hello World
+    }
+
+
+}
